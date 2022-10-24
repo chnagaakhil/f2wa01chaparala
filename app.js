0ff22ed421310105a824db90c709779485bc266b
Chaparala <S549701@nwmissouri.edu>
2022-10-24 17:00:42 -0500

    BONUS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
+var computeRouter = require('./routes/computation');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/mydata', mydataRouter);
+app.use('/computation', computeRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
